cf00fdd1957a35c83d3931bcd148536fc926f194
sembarner22 <ildar1001@mail.ru>
2020-09-16 19:37:11 +0300

    Initial commit

---
+++ desktop/src/com/sembarner/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sembarner.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sembarner.TrLights;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TrLights(), config);
+	}
+}
